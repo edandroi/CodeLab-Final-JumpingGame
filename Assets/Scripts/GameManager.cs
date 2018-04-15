dea5c71e8ff04041a4f04ac79ff6964d29876038
Unknown <edanuramail@gmail.com>
2018-04-15 10:59:39 -0400

    Basic Functions: Jump, Switch Camera

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Camera camPlayer;
+	public Camera camScene;
+	public KeyCode changeCam;
+	bool camSwitch = false;
+
+	// Use this for initialization
+	void Start () {
+		
+		camPlayer.gameObject.SetActive(false);
+		camScene.gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (changeCam)) 
+		{
+			camSwitch = !camSwitch;
+			camPlayer.gameObject.SetActive (!camSwitch);
+			camScene.gameObject.SetActive (camSwitch);
+		}
+//		if (camPlayer.gameObject.SetActive == false) 
+//		{
+//			camPlayer.gameObject.SetActive (true);
+//		} else 
+//		{
+//			camPlayer.gameObject.SetActive (false);
+//		}
+//
+//		if (camScene.gameObject.SetActive == true) {
+//			camScene.gameObject.SetActive (false);
+//		} else 
+//		{
+//			camScene.gameObject.SetActive (true);
+//		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+//	public KeyCode keyUp;
+//	public KeyCode keyDown;
+	public KeyCode keyRight;
+	public KeyCode keyLeft;
+	public float speed;
+	public float jumpSpeed;
+	float jumpSpeedStart;
+	public bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+		jumpSpeedStart = jumpSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(keyRight))
+		{
+			transform.position += new Vector3(1,0,0)*speed*Time.deltaTime;
+		}
+
+		if (Input.GetKey(keyLeft))
+		{
+			transform.position += new Vector3(-1,0,0)*speed*Time.deltaTime;
+		}
+
+		if (jump == true) 
+		{
+			GetComponent<Rigidbody> ().useGravity = false;
+			Jump ();
+		}
+
+		if (jump == false) 
+		{
+			GetComponent<Rigidbody> ().useGravity = true;
+			jumpSpeed = jumpSpeedStart;
+		}
+
+
+
+	}
+
+
+	void OnTriggerEnter ()
+	{
+		jump = true;
+	}
+
+	void Jump ()
+	{
+		transform.position += new Vector3(0,1,0)*jumpSpeed*Time.deltaTime;
+		jumpSpeed -= 0.1f;
+
+		if (jumpSpeed <= 0.5) {
+			jump = false;
+
+		}
+
+	}
+}
