13c23b32965487b909737c79bd62ae30dfc58d92
Unknown <edanuramail@gmail.com>
2018-05-04 12:16:09 -0400

    Score added

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+		public Text textbox;
+
+		void Start()
+		{
+			textbox = GetComponent<Text>();
+		}
+
+		void Update()
+		{
+		
+			textbox.text = "Score: " + GameManager.score;
+		}
+	}
